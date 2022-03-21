6ab19f429aa47525bfc7d0b07a5980ca35310f2d
Sahil Gupte <sahilsachingupte@gmail.com>
2022-03-22 03:14:35 +0530

    Remove playlist_songs from playlist model
    Signed-off-by: Sahil Gupte <sahilsachingupte@gmail.com>

--- models.d.ts
+++ models.d.ts
@@ -26,7 +26,6 @@ export interface Playlist {
   playlist_id: string
   playlist_name: string
   playlist_coverPath: string | undefined
-  playlist_songs?: Song[]
   playlist_song_count?: number
   playlist_path?: string
   isRemote?: boolean
